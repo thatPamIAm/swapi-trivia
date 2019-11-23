9c01011db83fbde365f93c780298c566a93b0383
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-11-22 21:08:22 -0700

    Add App snapshots
    Add testing for App related to udpates in Login
    
    Fix formatting
    
    Add snapshots for Card;

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-it('renders without crashing', () => {
 
+describe('App', () => {
+  let wrapper;
+  const historyMock = { push: jest.fn() };
+
+
+  beforeEach(() => {
+    wrapper = shallow(<App.WrappedComponent history={historyMock} />)
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  });
+
+  it('should have a default state', () => {
+
+    expect(wrapper.state().userInfo).toBe(null);
+  });
+
+  it('should update a user\'s info when logInUser is invoked', () => {
+    const user = {
+      fields: {
+        user: 'Darth Vader',
+        quote: 'I think, therefore I am',
+        ranking: 'Expert'
+      }
+    }
+
+    expect(wrapper.state().userInfo).toBe(null);
+    wrapper.instance().logInUser(user);
+    expect(wrapper.state().userInfo).toBe(user);
+  });
 });
