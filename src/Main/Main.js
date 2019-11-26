8c191f73f14b6a8bf8181df349ca9e1cb6870694
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-11-26 16:45:26 -0700

    Move methods and state from Movies to Main
    Rename JS, CSS, test files from Movies to CardContainer
    
    Update testing in Main to reflect class component
    
    Rename files from Movies to CardContainer

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'; 
 import Login from '../Login/Login';
-import Movies from '../Movies/Movies';
+import CardContainer from '../CardContainer/CardContainer';
 
-function Main(props) {
-  const {logInUser} = props;
+class Main extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+      characters: null
+    }
+  }
 
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/">
-          <Login logInUser={logInUser} />
-        </Route>
-        <Route path="/movies">
-          <Movies />
-        </Route>
-      </Switch>
-    </div>
-  )
+  componentDidMount(){
+    this.fetchMovies('https://swapi.co/api/films/');
+  }
 
+  async fetchMovies(url) {
+    let data = await fetch(url);
+    data = await data.json();
+
+    data = data.results.map((result) => {
+      const last = result.release_date.substring(0, 4);
+      const first = result.release_date.substring(5, 10);
+
+      result.release_date = first + '-' + last;
+      return result;
+    });
+
+    data.sort((a, b) => a.episode_id - b.episode_id);
+
+    this.setState({
+      movies: data
+    });
+  }
+
+  render() {
+    const {logInUser} = this.props;
+
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Login logInUser={logInUser} />
+          </Route>
+          <Route path="/movies">
+            <CardContainer movies={this.state.movies} />
+          </Route>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default Main;
--- src/Main/Main.test.js
+++ src/Main/Main.test.js
@@ -5,10 +5,18 @@ import Main from './Main';
 
 describe('Main', () => {
   let mockLogin = jest.fn();
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Main logInUser={mockLogin}/>);
+  });
 
   it('should match the snapshot', () => {
-    let wrapper = shallow(<Main logInUser={mockLogin}/>);
-    
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should have a default state', () => {
+    expect(wrapper.state().movies).toBe(null);
+    expect(wrapper.state().characters).toBe(null);
   });
 });
